2b0bb939d48c58adfade231de02e59d442b55762
yavuz <yavuz@farmazon.com.tr>
2020-03-23 01:31:20 +0300

    Add project files.

---
+++ XUnitTest.WEB/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XUnitTest.WEB.Models;
+
+namespace XUnitTest.WEB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
